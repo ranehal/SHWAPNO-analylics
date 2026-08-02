7f22596a73d9581f3fd10f2f48e6c2e47e9efced
ranx-x <educational.purpose37@gmail.com>
2026-08-02 22:17:04 +0000

    if this works ill get some sleep frfr 2026-08-03 04:17:04

--- frontend/shwapno_manifest.js
+++ frontend/shwapno_manifest.js
@@ -3,7 +3,7 @@ window.shwapno_manifest = {
   "app_id": "com.shwapno",
   "api_base": "https://store-api.shwapno.com/en/api",
   "headers": {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3 Safari/605.1.15",
     "Accept": "application/json",
     "client-type": "App",
     "customer": "fbbbb451-a780-4101-88e0-aad799e60f83",
@@ -741,5 +741,5 @@ window.shwapno_manifest = {
   ],
   "total_categories": 36,
   "total_products": 3463,
-  "captured_at": "2026-08-02T21:15:29.080190"
+  "captured_at": "2026-08-02T22:17:00.532617"
 };
