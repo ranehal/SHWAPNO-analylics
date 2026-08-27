7dac3bcf6a750eebc45aa469533536be28913d52
ranehal <ranehal@users.noreply.github.com>
2026-08-27 10:35:58 +0000

    if this works ill get some sleep frfr 2026-08-27 16:35:58

--- shwapno_manifest.js
+++ shwapno_manifest.js
@@ -3,7 +3,7 @@ window.shwapno_manifest = {
   "app_id": "com.shwapno",
   "api_base": "https://store-api.shwapno.com/en/api",
   "headers": {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:122.0) Gecko/20100101 Firefox/122.0",
     "Accept": "application/json",
     "client-type": "App",
     "customer": "fbbbb451-a780-4101-88e0-aad799e60f83",
@@ -733,5 +733,5 @@ window.shwapno_manifest = {
   ],
   "total_categories": 36,
   "total_products": 3375,
-  "captured_at": "2026-08-25T21:22:32.962818"
+  "captured_at": "2026-08-27T10:35:58.292842"
 };
